e2a1b2cb9c6ace9e32f4726da569e68c910b6162
Ktistos <ktistkostas@gmail.com>
2021-03-13 22:25:34 +0200

    another failed version

--- Project.java
+++ Project.java
@@ -1,18 +1,16 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class Project {
 
@@ -20,9 +18,9 @@ public class Project {
         public static void main(String[] args) {
     
             
-            InputStream inputStream=null;
+            BufferedReader inputStream=null;
             try {
-                inputStream = new BufferedInputStream(new FileInputStream(args[0]));
+                inputStream = new BufferedReader(new FileReader(args[0]));
             } catch (FileNotFoundException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -36,24 +34,29 @@ public class Project {
             }
 
 
-            InputReader in = new InputReader(inputStream);
+          
             PrintWriter fileOut = new PrintWriter(outputStream);
             Output out =  new Output(fileOut);
-            Experiment exp = new Experiment(in, out);
-            exp.experiment();
+            Experiment exp = new Experiment(inputStream, out);
+            try {
+                exp.experiment();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             fileOut.close();
         }
     
     
     static class Experiment{
-        InputReader in ;
+        BufferedReader in ;
         Output out;
         int numOfRoads;
         Vertex source;
         Vertex destination;
         HashMap<String,Edge> roads = new HashMap<>();
 
-        Experiment(InputReader in , Output out){
+        Experiment(BufferedReader in , Output out){
             this.in=in;
             this.out=out;
             numOfRoads=0;
@@ -62,59 +65,77 @@ public class Project {
 
         }
         
-        public void experiment(){
+        public void experiment() throws IOException{
             constructGraph();
-            out.println(roads.toString());
+        
         }
 
 
 
 
-        void constructGraph(){
+        void constructGraph() throws IOException{
             
             //aquiring and creating source vertex from file
-            String sourceName = in.next();
+            String sourceName = in.readLine();
             sourceName =  sourceName.substring(8,sourceName.length()-9);
             source= new Vertex(sourceName);
             
             //aquiring and creating destination vertex from file
-            String destinationName = in.next();
+            String destinationName = in.readLine();
             destinationName= destinationName.substring(13,destinationName.length()-14);
             destination = new Vertex(destinationName);
 
+            HashMap<String,Vertex> vertexMap = new HashMap<>();
+            vertexMap.put(sourceName, source);
+            vertexMap.put(destinationName, destination);
+
             //bypassing the <Roads> string in the file
-            in.next();
+            in.readLine();
             //reading all lines until line == </Roads> to get the graph info
-            String fileLine=in.next().replaceAll(" ", "");
+            String fileLine=in.readLine().replaceAll(" ", "");
             while(!fileLine.equals("</Roads>")){
                 String[] lineTokens =  fileLine.split(";");
-                out.println(lineTokens.toString());
-                String roadName = lineTokens[0];
+                
+                addToGraph(lineTokens, vertexMap);
+
+                fileLine=in.readLine().replaceAll(" ", "");
+            }
+            for(String s : vertexMap.keySet()){
+                out.print(s + " " );
+                for(int i=0; i<vertexMap.get(s).edges.size();i++){
+                    out.print(vertexMap.get(s).edges.get(i).name + " ");
+                }
+                out.println("");
+            }
+        }
+
+        void addToGraph(String[] graphInfo,HashMap<String,Vertex> vertexMap){
+
+                String roadName = graphInfo[0];
+                String startVertexName  = graphInfo[1];
+                String endVertexName= graphInfo[2];
+                float normalWeight = Float.parseFloat(graphInfo[3]);
+
                 Vertex startingVertex;
-                if(lineTokens[1].equals(sourceName))    
-                    startingVertex=source;
-                else
-                    startingVertex = new Vertex(lineTokens[1]);
-                
+                if(vertexMap.containsKey(startVertexName))    
+                    startingVertex=vertexMap.get(startVertexName);
+                else{
+                    startingVertex = new Vertex(startVertexName);
+                    vertexMap.put(startVertexName, startingVertex);
+                }
                 Vertex endVertex;
-                if(lineTokens[2].equals(destinationName))
+                if(vertexMap.containsKey(endVertexName))
                     endVertex=destination;
-                else
-                    endVertex = new Vertex(lineTokens[2]);
-                
-                float normalWeight = Float.parseFloat(lineTokens[3]);
-                
+                else{
+                    endVertex = new Vertex(endVertexName);
+                    vertexMap.put(endVertexName, endVertex);
+                }
                 Edge road = new Edge(roadName,startingVertex,endVertex,normalWeight);
                 
                 roads.put(roadName,road);
 
                 startingVertex.addEdge(road);
                 endVertex.addEdge(road);
-
-                fileLine=in.next().replaceAll(" ", "");
-            }
-
-            
         }
     }
     
@@ -146,32 +167,7 @@ public class Project {
         }
     }
 
-    static class InputReader {
-        public BufferedReader reader;
-        public StringTokenizer tokenizer;
-    
-        public InputReader(InputStream stream) {
-            reader = new BufferedReader(new InputStreamReader(stream), 32768);
-            tokenizer = null;
-        }
-        
-        public String next() {
-            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    tokenizer = new StringTokenizer(reader.readLine());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return tokenizer.nextToken();
-        }
-    
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-
-    }
+   
 
     static class Output{
         PrintWriter fileOut;
