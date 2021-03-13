04adaca7e272a10d5931e0b5e90a794830c1e951
Ktistos <ktistkostas@gmail.com>
2021-03-13 21:16:13 +0200

    failed version

--- Project.java
+++ Project.java
@@ -9,6 +9,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Project {
@@ -36,65 +39,157 @@ public class Project {
             InputReader in = new InputReader(inputStream);
             PrintWriter fileOut = new PrintWriter(outputStream);
             Output out =  new Output(fileOut);
-            Experiment exp = new Experiment();
-            exp.experiment(in, out);
+            Experiment exp = new Experiment(in, out);
+            exp.experiment();
             fileOut.close();
         }
     
     
     static class Experiment{
-        public void experiment(InputReader in , Output out){
+        InputReader in ;
+        Output out;
+        int numOfRoads;
+        Vertex source;
+        Vertex destination;
+        HashMap<String,Edge> roads = new HashMap<>();
+
+        Experiment(InputReader in , Output out){
+            this.in=in;
+            this.out=out;
+            numOfRoads=0;
+            source=null;
+            destination= null ;
+
+        }
+        
+        public void experiment(){
+            constructGraph();
+            out.println(roads.toString());
+        }
+
+
+
+
+        void constructGraph(){
+            
+            //aquiring and creating source vertex from file
+            String sourceName = in.next();
+            sourceName =  sourceName.substring(8,sourceName.length()-9);
+            source= new Vertex(sourceName);
+            
+            //aquiring and creating destination vertex from file
+            String destinationName = in.next();
+            destinationName= destinationName.substring(13,destinationName.length()-14);
+            destination = new Vertex(destinationName);
+
+            //bypassing the <Roads> string in the file
+            in.next();
+            //reading all lines until line == </Roads> to get the graph info
+            String fileLine=in.next().replaceAll(" ", "");
+            while(!fileLine.equals("</Roads>")){
+                String[] lineTokens =  fileLine.split(";");
+                out.println(lineTokens.toString());
+                String roadName = lineTokens[0];
+                Vertex startingVertex;
+                if(lineTokens[1].equals(sourceName))    
+                    startingVertex=source;
+                else
+                    startingVertex = new Vertex(lineTokens[1]);
+                
+                Vertex endVertex;
+                if(lineTokens[2].equals(destinationName))
+                    endVertex=destination;
+                else
+                    endVertex = new Vertex(lineTokens[2]);
+                
+                float normalWeight = Float.parseFloat(lineTokens[3]);
+                
+                Edge road = new Edge(roadName,startingVertex,endVertex,normalWeight);
+                
+                roads.put(roadName,road);
+
+                startingVertex.addEdge(road);
+                endVertex.addEdge(road);
+
+                fileLine=in.next().replaceAll(" ", "");
+            }
+
             
         }
     }
     
+    static class Vertex{
+        String name;
+        List<Edge> edges;
+        
+        Vertex(String name){
+            this.name=name;
+            edges= new LinkedList<>();
+        }
+
+        void addEdge(Edge edge){
+            edges.add(edge);
+        }
+
+    }
     
+    static class Edge{
+        String name;
+        float normalWeight;
+        Vertex start,end;
+
+        Edge(String name,Vertex start,Vertex end,float normalWeight){
+            this.name= name;
+            this.start=start;
+            this.end=end;
+            this.normalWeight= normalWeight;
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
     
-        static class InputReader {
-            public BufferedReader reader;
-            public StringTokenizer tokenizer;
-     
-            public InputReader(InputStream stream) {
-                reader = new BufferedReader(new InputStreamReader(stream), 32768);
-                tokenizer = null;
-            }
-            
-            public String next() {
-                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                    try {
-                        tokenizer = new StringTokenizer(reader.readLine());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+        
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                return tokenizer.nextToken();
             }
-     
-            public int nextInt() {
-                return Integer.parseInt(next());
-            }
+            return tokenizer.nextToken();
+        }
     
-    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+
     }
 
     static class Output{
-        PrintWriter consoleOut;
         PrintWriter fileOut;
         
         Output(PrintWriter  fileArg){
-            fileOut= new PrintWriter(fileArg);
-            consoleOut = fileArg;
+            fileOut= fileArg;
+    
         }
 
 
         void println(Object argument){
             fileOut.println(argument);
-            consoleOut.println(argument);
+            System.out.println(argument);
         }
 
         void print(Object argument){
             fileOut.print(argument);
-            consoleOut.print(argument);
+            System.out.print(argument);
         }
 
     }
